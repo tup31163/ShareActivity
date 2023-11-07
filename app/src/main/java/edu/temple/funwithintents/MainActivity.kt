97aee120dd81b3189a39e2c89263449dcdf32496
Kenny Miller <63763541+knnymllr@users.noreply.github.com>
2023-11-07 14:44:09 -0500

    one line setOnClickListener start new implicit intent

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,5 +1,6 @@
 package edu.temple.funwithintents
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
 
         // When the user clicks this button, share the text
         findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
-
+            startActivity(Intent(Intent.ACTION_SEND).setType("text/plain").putExtra(Intent.EXTRA_TEXT, editText.text.toString()))
         }
     }
 }
